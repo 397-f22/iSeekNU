1cfc29a1d7d1f531597f35536b51c980ad29ae1a
Kangcheng Li <kangcheng@glodonusa.com>
2022-11-18 22:04:16 -0600

    fix disappearing circle

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -1,26 +1,26 @@
-import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
-import "./map.css"
-
-const libraries = ["places"]
+import { GoogleMap, Circle, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import React from 'react';
 
 export default function Map() {
     // Loads the map using API KEY
-    const { isLoaded } = useLoadScript({
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
         googleMapsApiKey: "",
-        libraries
     });
-    // This returns while map is being loaded
-    if (!isLoaded) return <div>Loading...</div>
-    return (
+
+    return isLoaded ? (
         <GoogleMap 
             zoom={17}
             center={{ lat: 42.0565, lng: -87.6753 }} 
             mapContainerClassName='map-container'
         >
-            <MarkerF icon = "https://toppng.com/uploads/preview/red-circle-1155276042606ekqvli9k.png" shape = {"circle"} opacity = {.5} position={{ lat: 42.0565, lng: -87.6753 }} />
-            {/* <Circle visible = {true} center = {{ lat: 42.0565, lng: -87.6753 }} radius = {100} /> */}
+            {/* <MarkerF icon = {{ url: "https://cdn-icons-png.flaticon.com/512/5591/5591708.png",
+                     scaledSize:  new google.maps.Size(50,50) }}
+                     opacity = {.5} 
+                     position={{ lat: 42.0565, lng: -87.6753 }} /> */}
+            <Circle center = {{ lat: 42.0565, lng: -87.6753 }} radius = {100} />
         </GoogleMap>
-        )
+        ) : <></>
         //42.0451° N, 87.6877°W
 }
 
