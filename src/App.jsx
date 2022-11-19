3c828da8ffa52baaf574ee2298f0f0096854953c
Kangcheng Li <kangcheng@glodonusa.com>
2022-11-19 11:56:37 -0600

    use real location for marker, firebase data for circle

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import Map from './Components/map'
 
 import './App.css'
 
+
+
 function App() {
     return (
       <div>
         <Map/>
-        <button onClick = {()=> getLoc()} >coordinates</button>
       </div>
     )
 
 }
-const getLoc = () =>{
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-  });
-}
+
 
 export default App
--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -1,7 +1,27 @@
 import { GoogleMap, Circle, MarkerF, useJsApiLoader } from '@react-google-maps/api';
-import React from 'react';
+import React, { useState } from 'react';
+import { useDbUpdate, useDbData } from '../utilities/firebase';
+
+function getLoc(setLat, setLong, updateDb) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+        updateDb({1 : `${position.coords.latitude},${position.coords.longitude}`})
+        console.log(position.coords.latitude, position.coords.longitude);
+    });
+}
 
 export default function Map() {
+    const [longitude, setLongitude] = useState(-87.6753);
+    const [latitude, setLatitude] = useState(42.0565);
+
+    const [updateDb, result] = useDbUpdate("user/8888/hider")
+    const [data, error] = useDbData("user/8888/hider")
+    
+    // if (error) return <h1>Error loading data: {error.toString()}</h1>;
+    // if (data === undefined) return <h1>Loading data...</h1>;
+    // if (!data) return <h1>No data found</h1>;
+
     const options = {
         styles: [
             {
@@ -127,19 +147,33 @@ export default function Map() {
     });
 
     return isLoaded ? (
-        <GoogleMap 
-            zoom={17}
-            center={{ lat: 42.0565, lng: -87.6753 }} 
-            mapContainerClassName='map-container'
-            mapTypeId= 'terrain'
-            options={options}
-        >
-            {/* <MarkerF icon = {{ url: "https://cdn-icons-png.flaticon.com/512/5591/5591708.png",
-                     scaledSize:  new google.maps.Size(50,50) }}
-                     opacity = {.5} 
-                     position={{ lat: 42.0565, lng: -87.6753 }} /> */}
-            <Circle center = {{ lat: 42.0565, lng: -87.6753 }} radius = {100} />
-        </GoogleMap>
+        <div>
+            <GoogleMap 
+                zoom={16}
+                center={{ lat: 42.0565, lng: -87.6753 }} 
+                mapContainerClassName='map-container'
+                mapTypeId= 'terrain'
+                options={options}
+            >
+                <MarkerF icon = {{ url: "https://cdn-icons-png.flaticon.com/512/5591/5591708.png",
+                        scaledSize:  new google.maps.Size(70,70) }}
+                        opacity = {.9} 
+                        position={{ lat: latitude, lng: longitude }} />
+                {data?.map(loc => 
+                    {   
+                        const location = loc.split(',');
+                        return <Circle center = {{ lat: parseFloat(location[0]), lng: parseFloat(location[1]) }} radius = {80} />
+                    }
+                )}
+                {/* <Circle center = {{ lat: 42.0565, lng: -87.6753 }} radius = {80} /> */}
+                
+                {/* <Circle center = {{ lat: latitude, lng: longitude }} radius = {50} /> */}
+            </GoogleMap>
+            <div style={{display: "flex", justifyContent: "center", marginTop: "-70px"}}>
+                <button style={{zIndex: "1"}} onClick = {()=> getLoc(setLatitude, setLongitude, updateDb)} >Done hiding</button>
+                <button style={{zIndex: "1"}} onClick = {()=> getLoc(setLatitude, setLongitude, updateDb)} >Locate Me</button>
+            </div>
+        </div>
         ) : <></>
         //42.0451° N, 87.6877°W
 }
