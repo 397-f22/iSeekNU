79637d7f8a9af7c832dab34dc1cf55c72939ace9
Dimitrios Mavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-11-30 13:50:25 -0600

    Pushing to main, minor fix
    Fix on the how to play pop up, added a new jsx and css page for a pop up template that can be copied and used throught the app

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,11 +1,12 @@
 import { useDbUpdate, useDbData } from "../utilities/firebase";
 import React, { useState } from "react";
 import RandomNum from "./RandomNum";
+import { HowToPlayPopup } from "./HowToPlayPopUp";
+
 
 const Homepage = ({ setRoomID, setHomepage, seeker, setSeeker }) => {
   const [submitGroup, result] = useDbUpdate("user/");
   const [data, error] = useDbData("user/");
-  const [instruction, setInstruction] = useState(false);
 
   const enterGroup = (evt) => {
     const group = document.getElementById("group").value;
@@ -21,16 +22,6 @@ const Homepage = ({ setRoomID, setHomepage, seeker, setSeeker }) => {
     }
   };
 
-  const getRandomNum = () => {
-    let random = Math.floor(Math.random() * 9000 + 1000);
-    if (data) {
-      while (Object.keys(data).includes(random)) {
-        random = Math.floor(Math.random() * 9000 + 1000);
-      }
-    }
-    return random;
-  }
-
   const createGroup = () => {
     let random = document.getElementById("randomNum").innerText;
     setRoomID(random);
@@ -45,7 +36,11 @@ const Homepage = ({ setRoomID, setHomepage, seeker, setSeeker }) => {
   const random = <RandomNum data={data}/>
   // const random = getRandomNum(data)
 
+  const [open, setOpen] = useState(false);
+
+
   return (
+    
     <div style={{ marginTop: "50px" }}>
       <div style={{ textAlign: "center"}}>
         <p style={{fontSize: "20px"}}>Your Group Number</p>
@@ -89,34 +84,14 @@ const Homepage = ({ setRoomID, setHomepage, seeker, setSeeker }) => {
         >
           <span style={{color: seeker ? "#213547":  "white" }}>Hider</span>
         </button>
-      </div>
-
-      <div style={{display: "flex", justifyContent: "center"}}>
-      {instruction && <div style={{position: "absolute", transform: "translateY(-100%)"}}>
-        <div style={{textAlign: "left", width: "75vw", border: "0.5px solid black", borderRadius: "5px", padding: "10px 20px 10px 20px", backgroundColor: "white"}}>
-          <ul>
-            <li style={{fontSize: "17px"}}> Allow location services to be on and give permission to this app.</li>
-            <li style={{fontSize: "17px"}}> Choose a seeker from your group of players. The seeker will choose the seeker role and click on the Create Group button.</li>
-            <li style={{fontSize: "17px"}}> Share the group number with your group of players to use to join group. Simply enter the given code and join the room.</li>
-            <li style={{fontSize: "17px"}}> For the next 20 minutes, the seeker will stay in one place while the hiders move around campus to find a hiding spot. Use the Locate Me to help navigate around the campus.</li>
-            <li style={{fontSize: "17px"}}> When the hiders have found a hiding spot, the hider will click on the Done Hiding Button to create a circle around their location.</li>
-            <li style={{fontSize: "17px"}}> The hiders may not move away from their designated circle.</li>
-            <li style={{fontSize: "17px"}}> At the end of 20 minutes, the seeker will go find the hiders using the circles as a guide.</li>
-            <li style={{fontSize: "17px"}}> When the seeker finds a person, the seeker will click on the circle to remove it from play. Once all circles have been removed, the game ends.</li>
-          </ul>
+        <div>
+        <br></br>
+        <button onClick={() => setOpen(true)}>How to Play?</button>
+        {open ? <HowToPlayPopup closePopup={() => setOpen(false)} /> : null}
         </div>
-      </div>}
-      </div>
-      
-      <div className="question" style={{display: "flex", justifyContent: "center", alignItems: "center", marginTop: "30px"}}>
-        <span style={{fontSize: "20px"}}>How to play&nbsp;</span>
-          <img 
-              src="https://cdn-icons-png.flaticon.com/512/18/18436.png" 
-              style={{height: "30px", zIndex: "100"}}
-              onClick={() => setInstruction(!instruction)}/>
-      </div>
+        <br></br>
 
-      
+      </div> 
     </div>
   );
 };
---
+++ src/Components/HowToPlayPopUp.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./HowToPlayPopup.css";
+export const HowToPlayPopup = ({ closePopup }) => {
+  return (
+      <div style={{display: "flex", justifyContent: "center"}}>
+      <div style={{position: "absolute", transform: "translateY(-75%)"}}>
+        <div style={{textAlign: "center", width: "80vw", border: "0.5px solid black", borderRadius: "5px", padding: "5px 10px 5px 10px", backgroundColor: "black", opacity:0.95}}>
+        <div style={{display: "flex", justifyContent: "right"}}>
+        <button onClick={closePopup}>Close X</button>
+        </div>
+            <h1>Steps on how to play the game:</h1>
+            <ul style={{textAlign: "center", listStylePosition: "inside"}}>
+                <li style={{fontSize: "17px"}}> Allow location services to be on and give permission to this app.</li>
+                <li style={{fontSize: "17px"}}> Choose a seeker from your group of players. The seeker will choose the seeker role and click on the Create Group button.</li>
+                <li style={{fontSize: "17px"}}> Share the group number with your group of players to use to join group. Simply enter the given code and join the room.</li>
+                <li style={{fontSize: "17px"}}> For the next 20 minutes, the seeker will stay in one place while the hiders move around campus to find a hiding spot. Use the Locate Me to help navigate around the campus.</li>
+                <li style={{fontSize: "17px"}}> When the hiders have found a hiding spot, the hider will click on the Done Hiding Button to create a circle around their location.</li>
+                <li style={{fontSize: "17px"}}> The hiders may not move away from their designated circle.</li>
+                <li style={{fontSize: "17px"}}> At the end of 20 minutes, the seeker will go find the hiders using the circles as a guide.</li>
+                <li style={{fontSize: "17px"}}> When the seeker finds a person, the seeker will click on the circle to remove it from play. Once all circles have been removed, the game ends.</li>
+            </ul>
+      </div>
+     </div>
+    </div>
+  );
+};
