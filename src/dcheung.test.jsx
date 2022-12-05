9f522a3d0673d6e9c483fcb6c585b250a8fdb531
dcheung0720 <73008040+Railgun707@users.noreply.github.com>
2022-12-04 20:43:45 -0600

    test1

--- src/dcheung.test.jsx
+++ src/dcheung.test.jsx
@@ -4,3 +4,19 @@ import App from "./App"
 import Homepage from "./Components/Homepage"
 import Map from "./Components/map"
 
+describe("selection", ()=>{
+    it("shows the correct role", async()=>{
+        render(<App />);
+        await screen.findByText(/I am a Hider/)
+        })
+    }
+)
+
+describe("selection", ()=>{
+    it("changes to the correct role when clicked", async()=>{
+        const button = await screen.getByTestId("seeker");
+        button.click();
+        await screen.findByText("I am a Seeker");
+       })
+    }
+)
