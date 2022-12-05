56794724fcc96e4e711cd63e3fd92c905c787bd4
dcheung0720 <73008040+Railgun707@users.noreply.github.com>
2022-12-04 20:43:21 -0600

    tests

--- src/dcheung.test.jsx
+++ src/dcheung.test.jsx
@@ -4,33 +4,3 @@ import App from "./App"
 import Homepage from "./Components/Homepage"
 import Map from "./Components/map"
 
-describe("selection", ()=>{
-    it("shows the correct role", async()=>{
-        render(<App />);
-        await screen.findByText(/I am a Hider/)
-        })
-    }
-)
-
-describe("selection", ()=>{
-    it("changes to the correct role when clicked", async()=>{
-        const button = await screen.getByTestId("seeker");
-        button.click();
-        await screen.findByText("I am a Seeker");
-       })
-    }
-)
-
-// describe("enter room" , ()=>{
-//     it("should change the room to the entered room", async()=>{
-//         const input = await screen.getByTestId("input");
-//         input.innerHTML = 1335;
-//         input.value = 1335;
-
-//         await screen.findByText(/1335/);
-//         const button = await screen.getByTestId("join");
-//         button.click();
-//         console.log(button.value)
-//         await screen.findByText(/LOCATE/);
-//     })
-// })
