a009d074a6c74b818c9f56b30692c7bd8cb2cc55
dcheung0720 <73008040+Railgun707@users.noreply.github.com>
2022-12-04 21:21:28 -0600

    test1

--- src/dcheung.test.jsx
+++ src/dcheung.test.jsx
@@ -2,31 +2,31 @@ import { describe, expect, it } from 'vitest';
 import { getAllByText,getByText, render, screen } from '@testing-library/react';
 import App from "./App"
 
-// describe("selection", ()=>{
-//     it("shows the correct role", async()=>{
-//         render(<App />);
-//         await screen.findByText(/I am a Hider/)
-//         })
-//     }
-// )
+describe("selection", ()=>{
+    it("shows the correct role", async()=>{
+        render(<App />);
+        await screen.findByText(/I am a Hider/)
+        })
+    }
+)
 
-// describe("selection", ()=>{
-//     it("changes to the correct role when clicked (Success)", async()=>{
-//         const button = await screen.getByTestId("seeker");
-//         button.click();
-//         await screen.findByText("I am a Seeker");
-//        })
-//     }
-// )
+describe("selection", ()=>{
+    it("changes to the correct role when clicked (Success)", async()=>{
+        const button = await screen.getByTestId("seeker");
+        button.click();
+        await screen.findByText("I am a Seeker");
+       })
+    }
+)
 
 
-// describe("selection", ()=>{
-//     it("changes to the correct role when clicked (Fail)", async()=>{
-//         const button = await screen.getByTestId("seeker");
-//         button.click();
-//         const texts = await screen.queryAllByText("I am a Hider");
-//         //there shouldn't be such string because seeker is clicked
-//         expect(texts.length == 0).toBeTruthy();
-//        })
-//     }
-// )
+describe("selection", ()=>{
+    it("changes to the correct role when clicked (Fail)", async()=>{
+        const button = await screen.getByTestId("seeker");
+        button.click();
+        const texts = await screen.queryAllByText("I am a Hider");
+        //there shouldn't be such string because seeker is clicked
+        expect(texts.length == 0).toBeTruthy();
+       })
+    }
+)
