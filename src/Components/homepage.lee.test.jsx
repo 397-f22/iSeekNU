898be3ffb09dc4ccf4ab5fcab53bfc1c0d973f5a
joshlee-7 <joshualee721@gmail.com>
2022-12-06 15:42:53 -0600

    first test

---
+++ src/Components/homepage.lee.test.jsx
@@ -0,0 +1,20 @@
+import { beforeEach, describe, vi, it, expect } from "vitest";
+import {
+  getAllByText,
+  getByText,
+  render,
+  screen,
+} from "@testing-library/react";
+import Hompage from "./Homepage";
+
+describe("how to play", () => {
+  it("should open a modal and allow you to close the page", async () => {
+    render(<Hompage />);
+    const btn = await screen.findByTestId("how");
+    btn.click();
+    await screen.queryAllByText(/Steps on how to play the game/);
+
+    // render(<Map roomID = {Object.entries(mockData.user)[0][0]} setHomepage = {null} seeker = {"seeker"}/>)
+    // await screen.findByText(/Join code/i);
+  });
+});
